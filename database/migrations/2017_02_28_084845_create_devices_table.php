6a18956bd981e4f13b7beec53a07e61cb7bf4c13
Mathias Ducancel <mathiasduc@gmail.com>
2017-02-28 10:23:29 +0100

    Initial commit:

---
+++ database/migrations/2017_02_28_084845_create_devices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('hexaID', 10);
+            $table->timestamps();
+            $table->string('location');
+            $table->time('installed_at');
+            $table->string('name', 50);
+            $table->string('comment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
